c8a220d24c38ed4209228b5f159be9e1adf0ceb7
hipawudi <abc95175346@hotmail.com>
2023-08-15 15:13:34 +0800

    feat:update about

--- nuxt/components/AppHeader.vue
+++ nuxt/components/AppHeader.vue
@@ -12,12 +12,14 @@ export default {
       openedMenu: false,
       menu: [
         { title: "HOME", link: "/" },
-        { title: "ORGANISATION", link: "/organisation" },
-        { title: "NEWS", link: "/news" },
-        { title: "PHOTO GALLERY", link: "/" },
-        { title: "EVENTS", link: "/" },
-        { title: "REGULATION", link: "/" },
-        { title: "RESULT", link: "/" },
+        { title: "NEWS", link: "/jua-news" },
+        { title: "JUA EC", link: "/executive-committee" },
+        { title: "JUA MEMBERS", link: "/jua-members" },
+        { title: "PHOTO GALLERY", link: "/gallery" },
+        { title: "EVENT", link: "/event" },
+        { title: "REGULATION", link: "/regulation" },
+        { title: "RESULT", link: "/result" },
+        { title: "ABOUT", link: "/about" },
         { title: "CONTACT US", link: "/contact" },
       ],
     };
@@ -156,53 +158,7 @@ export default {
                 <div class="text-white text-xs">NEWS</div>
               </div>
             </a>
-            <a href="/about">
-              <div
-                class="h-20 flex flex-col justify-center items-center p-3"
-                :class="route.matched[0].path.slice(1) == 'about' ? 'bg-[#2e99b0]' : ''"
-              >
-                <svg
-                  width="800px"
-                  height="800px"
-                  viewBox="0 0 512 512"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  fill="#ffffff"
-                >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0" />
 
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-
-                  <g id="SVGRepo_iconCarrier">
-                    <title>about</title>
-                    <g
-                      id="Page-1"
-                      stroke="none"
-                      stroke-width="1"
-                      fill="none"
-                      fill-rule="evenodd"
-                    >
-                      <g
-                        id="about-white"
-                        fill="#ffffff"
-                        transform="translate(42.666667, 42.666667)"
-                      >
-                        <path
-                          d="M213.333333,3.55271368e-14 C95.51296,3.55271368e-14 3.55271368e-14,95.51168 3.55271368e-14,213.333333 C3.55271368e-14,331.153707 95.51296,426.666667 213.333333,426.666667 C331.154987,426.666667 426.666667,331.153707 426.666667,213.333333 C426.666667,95.51168 331.154987,3.55271368e-14 213.333333,3.55271368e-14 Z M213.333333,384 C119.227947,384 42.6666667,307.43872 42.6666667,213.333333 C42.6666667,119.227947 119.227947,42.6666667 213.333333,42.6666667 C307.44,42.6666667 384,119.227947 384,213.333333 C384,307.43872 307.44,384 213.333333,384 Z M240.04672,128 C240.04672,143.46752 228.785067,154.666667 213.55008,154.666667 C197.698773,154.666667 186.713387,143.46752 186.713387,127.704107 C186.713387,112.5536 197.99616,101.333333 213.55008,101.333333 C228.785067,101.333333 240.04672,112.5536 240.04672,128 Z M192.04672,192 L234.713387,192 L234.713387,320 L192.04672,320 L192.04672,192 Z"
-                          id="Shape"
-                        ></path>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-                <div class="text-white text-xs">ABOUT</div>
-              </div>
-            </a>
             <a href="/executive-committee">
               <div
                 class="h-20 flex flex-col justify-center items-center p-3"
@@ -383,6 +339,53 @@ export default {
                   ></path>
                 </svg>
                 <div class="text-white text-xs">RESULT</div>
+              </div>
+            </a>
+            <a href="/about">
+              <div
+                class="h-20 flex flex-col justify-center items-center p-3"
+                :class="route.matched[0].path.slice(1) == 'about' ? 'bg-[#2e99b0]' : ''"
+              >
+                <svg
+                  width="800px"
+                  height="800px"
+                  viewBox="0 0 512 512"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  fill="#ffffff"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+
+                  <g id="SVGRepo_iconCarrier">
+                    <title>about</title>
+                    <g
+                      id="Page-1"
+                      stroke="none"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd"
+                    >
+                      <g
+                        id="about-white"
+                        fill="#ffffff"
+                        transform="translate(42.666667, 42.666667)"
+                      >
+                        <path
+                          d="M213.333333,3.55271368e-14 C95.51296,3.55271368e-14 3.55271368e-14,95.51168 3.55271368e-14,213.333333 C3.55271368e-14,331.153707 95.51296,426.666667 213.333333,426.666667 C331.154987,426.666667 426.666667,331.153707 426.666667,213.333333 C426.666667,95.51168 331.154987,3.55271368e-14 213.333333,3.55271368e-14 Z M213.333333,384 C119.227947,384 42.6666667,307.43872 42.6666667,213.333333 C42.6666667,119.227947 119.227947,42.6666667 213.333333,42.6666667 C307.44,42.6666667 384,119.227947 384,213.333333 C384,307.43872 307.44,384 213.333333,384 Z M240.04672,128 C240.04672,143.46752 228.785067,154.666667 213.55008,154.666667 C197.698773,154.666667 186.713387,143.46752 186.713387,127.704107 C186.713387,112.5536 197.99616,101.333333 213.55008,101.333333 C228.785067,101.333333 240.04672,112.5536 240.04672,128 Z M192.04672,192 L234.713387,192 L234.713387,320 L192.04672,320 L192.04672,192 Z"
+                          id="Shape"
+                        ></path>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+                <div class="text-white text-xs">ABOUT</div>
               </div>
             </a>
             <a href="/contact">
