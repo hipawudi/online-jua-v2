a5c4702746fc0d1a0b62a3c74ab702e49334f73e
hipawudi <abc95175346@hotmail.com>
2023-08-21 19:34:56 +0800

    feat:member

--- nuxt/pages/jua-members/[id].vue
+++ nuxt/pages/jua-members/[id].vue
@@ -28,23 +28,33 @@ export default {
         <div class="mb-4">
           <img :src="member.name_image" class="w-40" />
         </div>
-        <div class="flex gap-6">
+        <div class="flex flex-col md:flex-row gap-6">
           <div class="flex flex-col text-gray-500 text-lg">
-            <div>Title</div>
-            <div class="">Address</div>
-            <div class="">Phone</div>
-            <div class="">Web</div>
-            <div class="">Email</div>
+            <div class="flex gap-6">
+              <div class="w-16">Title</div>
+              <div class="text-black font-bold">{{ member.title }}</div>
+            </div>
+            <div class="flex gap-6">
+              <div class="w-16">Address</div>
+              <div class="text-black font-bold">{{ member.address }}</div>
+            </div>
+            <div class="flex gap-6">
+              <div class="w-16">Phone</div>
+              <div class="text-black font-bold">{{ member.phone }}</div>
+            </div>
+            <div class="flex gap-6">
+              <div class="w-16">Web</div>
+              <div class="text-black font-bold">{{ member.web }}</div>
+            </div>
+            <div class="flex gap-6">
+              <div class="w-16">Email</div>
+              <div class="text-black font-bold">{{ member.email }}</div>
+            </div>
           </div>
-          <div class="flex flex-col text-lg font-bold">
-            <div class="">{{ member.title }}</div>
-            <div class="">{{ member.address }}</div>
-            <div class="">{{ member.phone }}</div>
-            <div>{{ member.web }}</div>
-            <div>{{ member.email }}</div>
-          </div>
-          <div class="grow text-right">
-            <div class="flex justify-end gap-6">
+          <div class="grow md:text-right text-left">
+            <div
+              class="flex md:justify-end justify-start gap-6 md:flex-row flex-row-reverse"
+            >
               <div class="flex flex-col">
                 <div class="text-lg text-gray-500">President</div>
                 <div class="text-lg font-bold">{{ member.president }}</div>
